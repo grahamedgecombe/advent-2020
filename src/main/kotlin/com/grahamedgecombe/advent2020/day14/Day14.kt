f6eed17fe5e9240e6ab83a0a532292d8037e19ca
Graham Edgecombe <gpe@grahamedgecombe.com>
2020-12-14 09:22:32 +0000

    Add day 14 part 2 solution

--- src/main/kotlin/com/grahamedgecombe/advent2020/day14/Day14.kt
+++ src/main/kotlin/com/grahamedgecombe/advent2020/day14/Day14.kt
@@ -6,10 +6,26 @@ object Day14 : Puzzle<List<Day14.Instruction>>(14) {
     sealed class Instruction {
         data class Mask(
             val zeroes: Long,
-            val ones: Long
+            val ones: Long,
+            val floatingBits: Set<Int>
         ) : Instruction() {
             fun maskValue(value: Long): Long {
                 return (value or ones) and zeroes.inv()
+            }
+
+            fun maskAddress(value: Long): Set<Long> {
+                var values = setOf(value or ones)
+
+                for (bit in floatingBits) {
+                    val nextValues = mutableSetOf<Long>()
+                    for (v in values) {
+                        nextValues += v or (1L shl bit)
+                        nextValues += v and (1L shl bit).inv()
+                    }
+                    values = nextValues
+                }
+
+                return values
             }
         }
         data class Write(val address: Long, val value: Long) : Instruction()
@@ -23,18 +39,19 @@ object Day14 : Puzzle<List<Day14.Instruction>>(14) {
                 if (match != null) {
                     var zeroes = 0L
                     var ones = 0L
+                    val floating = mutableSetOf<Int>()
 
                     for ((i, char) in match.groupValues[1].withIndex()) {
                         val bit = 35 - i
                         when (char) {
                             '0' -> zeroes = zeroes or (1L shl bit)
                             '1' -> ones = ones or (1L shl bit)
-                            'X' -> Unit
+                            'X' -> floating += bit
                             else -> throw IllegalArgumentException()
                         }
                     }
 
-                    return Mask(zeroes, ones)
+                    return Mask(zeroes, ones, floating)
                 }
 
                 match = WRITE_REGEX.matchEntire(s) ?: throw IllegalArgumentException(s)
@@ -59,6 +76,24 @@ object Day14 : Puzzle<List<Day14.Instruction>>(14) {
             when (insn) {
                 is Instruction.Mask -> mask = insn
                 is Instruction.Write -> memory[insn.address] = mask.maskValue(insn.value)
+            }
+        }
+
+        return memory.values.sum()
+    }
+
+    override fun solvePart2(input: List<Instruction>): Long {
+        val memory = mutableMapOf<Long, Long>()
+        lateinit var mask: Instruction.Mask
+
+        for (insn in input) {
+            when (insn) {
+                is Instruction.Mask -> mask = insn
+                is Instruction.Write -> {
+                    for (address in mask.maskAddress(insn.address)) {
+                        memory[address] = insn.value
+                    }
+                }
             }
         }
 
--- src/test/kotlin/com/grahamedgecombe/advent2020/day14/Day14Test.kt
+++ src/test/kotlin/com/grahamedgecombe/advent2020/day14/Day14Test.kt
@@ -16,4 +16,15 @@ object Day14Test {
         ))))
         assertEquals(14553106347726, Day14.solvePart1(PROD_INPUT))
     }
+
+    @Test
+    fun testPart2() {
+        assertEquals(208, Day14.solvePart2(Day14.parse(sequenceOf(
+            "mask = 000000000000000000000000000000X1001X",
+            "mem[42] = 100",
+            "mask = 00000000000000000000000000000000X0XX",
+            "mem[26] = 1"
+        ))))
+        assertEquals(2737766154126, Day14.solvePart2(PROD_INPUT))
+    }
 }
